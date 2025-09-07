af3f2036d3324e848dca8df24c439d1d82395df6
nhgkhiem0307 <nhgiakhiem@gmail.com>
2025-09-07 20:34:02 +0700

    phan UI

---
+++ Assets/Scripts/PlayButton_Script.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; // cần cho SceneManager
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadSceneByName(string GameplayScene)
+    {
+        SceneManager.LoadScene(GameplayScene);
+    }
+
+    public void LoadSceneByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
