023b01524d69f9647739051cdac71fccd2f4a87a
nhgkhiem0307 <nhgiakhiem@gmail.com>
2025-09-07 21:38:43 +0700

    Upd nho truoc khi ngu

---
+++ Assets/Scripts/SoundToggle_Script.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicButtonToggle : MonoBehaviour
+{
+    public Sprite buttonMusic;      // sprite bật nhạc
+    public Sprite buttonMusicOff;   // sprite tắt nhạc
+
+    private Image buttonImage;
+    private bool isMusicOn = true; // trạng thái nhạc
+
+    void Start()
+    {
+        buttonImage = GetComponent<Image>();
+        buttonImage.sprite = buttonMusic; // ban đầu là bật
+    }
+
+    public void ToggleMusicButton()
+    {
+        if (isMusicOn)
+        {
+            buttonImage.sprite = buttonMusicOff; // đổi sang nút off
+            isMusicOn = false;
+            // ở đây bạn có thể thêm code tắt nhạc
+        }
+        else
+        {
+            buttonImage.sprite = buttonMusic; // đổi sang nút on
+            isMusicOn = true;
+            // ở đây bạn có thể thêm code bật nhạc
+        }
+    }
+}
