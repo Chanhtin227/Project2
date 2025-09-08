58d723a84581566c2e8fbdab55c0c3c6390c0825
nhgkhiem0307 <nhgiakhiem@gmail.com>
2025-09-08 21:31:17 +0700

    Upd UI

---
+++ Assets/Scripts/SettingManager_Script.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class SettingManager : MonoBehaviour
+{
+    [Header("Drag your Setting_Panel here")]
+    public GameObject settingPanel;
+
+    // Bật/tắt panel (cho Setting_Button)
+    public void ToggleSettingPanel()
+    {
+        if (settingPanel != null)
+        {
+            settingPanel.SetActive(!settingPanel.activeSelf);
+        }
+    }
+
+    // Chỉ đóng panel (cho button_close_0)
+    public void CloseSettingPanel()
+    {
+        if (settingPanel != null)
+        {
+            settingPanel.SetActive(false);
+            
+        }
+    }
+}
