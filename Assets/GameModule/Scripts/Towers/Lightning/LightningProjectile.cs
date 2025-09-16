f7d48aadf66a6b53b1822e882bb4b81f56a65b95
Nyx0806 <vankhanhs0384@gmail.com>
2025-09-16 17:50:43 +0700

    Sua 1 chut phan Lightning tower

--- Assets/GameModule/Scripts/Towers/Lightning/LightningProjectile.cs
+++ Assets/GameModule/Scripts/Towers/Lightning/LightningProjectile.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LightningProjectile : MonoBehaviour
@@ -10,35 +11,54 @@ public class LightningProjectile : MonoBehaviour
     public float jaggedness = 0.2f;
 
     [Header("Damage")]
-    public float damage = 10f;
     public LayerMask enemyLayer;
 
     private Coroutine running;
 
-    public void Initialize(Transform start, Transform target, float dmg, int chain, float radius, LayerMask enemyMask)
+    public void Initialize(Transform start, Transform target, float damage, int chains, float radius, LayerMask enemyMask, HashSet<Transform> alreadyHit = null)
     {
-        damage = dmg;
         enemyLayer = enemyMask;
 
         if (running != null) StopCoroutine(running);
-        running = StartCoroutine(DoLightning(start.position, target.position, target));
+        running = StartCoroutine(DoLightning(start.position, target.position, target, damage, chains, radius, alreadyHit));
     }
 
-    private IEnumerator DoLightning(Vector3 start, Vector3 end, Transform target)
+    private IEnumerator DoLightning(Vector3 start, Vector3 end, Transform target, float damage, int chains, float radius, HashSet<Transform> alreadyHit)
     {
         DrawZigZag(start, end);
         line.enabled = true;
 
-        if (target != null)
+        if (alreadyHit == null) alreadyHit = new HashSet<Transform>();
+        if (target != null && !alreadyHit.Contains(target))
         {
             enemy en = target.GetComponent<enemy>();
             if (en != null) en.TakeDamage((int)damage);
+            alreadyHit.Add(target);
         }
 
         yield return new WaitForSeconds(duration);
-
         line.enabled = false;
         running = null;
+
+        // Chain to next targets
+        if (chains > 0 && target != null)
+        {
+            Collider2D[] hits = Physics2D.OverlapCircleAll(target.position, radius, enemyLayer);
+            int chainsLeft = chains;
+            foreach (var hit in hits)
+            {
+                if (chainsLeft <= 0) break;
+                if (hit.transform == target || alreadyHit.Contains(hit.transform)) continue;
+
+                // Spawn new lightning for each chain
+                LightningProjectile zap = PoolManager.Instance.Get<LightningProjectile>("Lightning");
+                if (zap != null)
+                {
+                    zap.Initialize(target, hit.transform, damage, chains - 1, radius, enemyLayer, alreadyHit);
+                    chainsLeft--;
+                }
+            }
+        }
 
         PoolManager.Instance.Return(gameObject, "Lightning");
     }
