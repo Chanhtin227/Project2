2f09b1ca64b486f1b3b2626dd1c57f95bfcf6b1f
Chanhtin227 <lenguyenchanhtin206@gmail.com>
2025-09-19 17:25:36 +0700

    Tin Update

--- Assets/GameModule/Scripts/BaseEnemies/MultiPathCheckpointsManager.cs
+++ Assets/GameModule/Scripts/BaseEnemies/MultiPathCheckpointsManager.cs
@@ -7,6 +7,8 @@ public class MultiPathCheckpointsManager : MonoBehaviour
     public Transform[] pathA;
     public Transform[] pathB;
     public Transform[] pathC;
+    public Transform[] pathD;
+    public Transform[] pathE;
 
     void Awake()
     {
@@ -20,6 +22,8 @@ public class MultiPathCheckpointsManager : MonoBehaviour
             case 0: return pathA;
             case 1: return pathB;
             case 2: return pathC;
+            case 3: return pathD;
+            case 4: return pathE;
         }
         return null;
     }
